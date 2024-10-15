1e6e7608db1a7913a261f7364fbcd1ee6596b736
Ibaqu <aquib.zt@gmail.com>
2024-10-15 16:22:03 -0400

    Refactor Solitaire class functions
    - Split the setup function into 3 for better readability

--- src/main/java/org/ibaqu/Solitaire.java
+++ src/main/java/org/ibaqu/Solitaire.java
@@ -8,14 +8,14 @@ public class Solitaire {
 
     private Deck deck;
     private List<List<Card>> tableau;
-    private List<Stack<Card>> foundations;
+    private List<Stack<Card>> foundation;
     private List<Card> stock;
     private List<Card> waste;
 
     public Solitaire() {
         deck = new Deck();
         tableau = new ArrayList<>();
-        foundations = new ArrayList<>();
+        foundation = new ArrayList<>();
         stock = new ArrayList<>();
         waste = new ArrayList<>();
 
@@ -25,19 +25,45 @@ public class Solitaire {
 
     // Set up the game
     private void setupGame() {
+        setupTableau();
+        setupFoundation();
+        setupStock();
+    }
+
+    private void setupTableau() {
+        // Iterate through seven tableau
         for (int i = 0; i < 7; i++) {
+
             List<Card> pile = new ArrayList<>();
+            // Add j number of cards for the i'th tableau
             for (int j = 0; j <= i; j++) {
-                Card topCard = deck.drawCard();
-                if(j == i) topCard.flip();
-                pile.add(topCard);
+                // Draw card
+                Card tableauCard = deck.drawCard();
+
+                // If card is intended to be the top of the tableau, flip it
+                if (j == i) {
+                    tableauCard.flip();
+                }
+
+                // Add the card to the pile
+                pile.add(tableauCard);
             }
+
+            // Add setup pile to the tableau
             tableau.add(pile);
         }
+    }
+
+    private void setupFoundation() {
+        // Create an empty stack of cards to represent a full suit
+        // TODO : Is it necessary to have a 'isSuit' function?
         for (int i = 0; i < 4; i++) {
             Stack<Card> suit = new Stack<>();
-            foundations.add(suit);
+            foundation.add(suit);
         }
+    }
+
+    private void setupStock() {
         stock.addAll(deck.getCards());
     }
 
