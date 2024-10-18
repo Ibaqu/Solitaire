ea9f468839bd1367c45e26810b46816ca05aca12
Ibaqu <aquib.zt@gmail.com>
2024-10-18 17:02:29 -0400

    Add TableauPile class to handle tableaus
    - Manage cards in tableau using linkedLists
    - Adds functions to manage cards in a tableau

--- src/main/java/org/ibaqu/TableauPile.java
+++ src/main/java/org/ibaqu/TableauPile.java
@@ -3,13 +3,25 @@ package org.ibaqu;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Tableau {
+public class TableauPile {
 
     private LinkedList<Card> faceDownCards = new LinkedList<>();
     private LinkedList<Card> faceUpCards = new LinkedList<>();
 
     public void addFaceUpCard(Card card) {
         faceUpCards.add(card);
+    }
+
+    public void addFaceDownCard(Card card) {
+        faceDownCards.add(card);
+    }
+
+    public List<Card> getFaceUpCards() {
+        return faceUpCards;
+    }
+
+    public List<Card> getFaceDownCards() {
+        return faceDownCards;
     }
 
     public Card removeTopFaceUpCard() {
@@ -19,11 +31,7 @@ public class Tableau {
     public void flipTopCard() {
         // If face down cards are not empty and the faceup card list is empty
         if (!faceDownCards.isEmpty() && faceUpCards.isEmpty()) {
-            faceUpCards.add(faceDownCards.removeLast());
+            faceUpCards.add(faceDownCards.removeLast().flip());
         }
-    }
-
-    public List<Card> getFaceUpCards() {
-        return faceUpCards;
     }
 }
